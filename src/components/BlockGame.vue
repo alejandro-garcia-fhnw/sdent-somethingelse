5557f08b6c309699fe76ae64de8fecfd2a757040
dev-ale <ale.iphone@gmail.com>
2020-10-26 16:57:16 +0100

    init block game

---
+++ src/components/BlockGame.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h1>BlockGame</h1>
+
+    <canvas 
+      ref="game" 
+      width="650" 
+      height="480" 
+      style="border: 1px solid black">
+    </canvas>
+    <p>
+      <button v-on:click="move('right')">Right</button>
+      <button v-on:click="move('left')">Left</button>
+      <button v-on:click="move('up')">Up</button>
+      <button v-on:click="move('down')">Down</button>
+    </p>
+  </div>
+</template>
+
+<script>
+import io from "socket.io-client"
+  export default {
+    name: 'BlockGame',
+    data() {
+      return {
+        socket: {},
+        context: {},
+        position: {
+          x: 0,
+          y: 0
+        }
+      }
+    },
+    //before View rendered
+    created() {
+      this.socket = io("http://localhost:3000")
+    },
+    //after View rendered
+    mounted() {
+      this.context = this.$refs.game.getContext("2d");
+      this.socket.on("position", data => {
+        this.position = data;
+        this.context.clearRect(0,0, this.$refs.game.width, this.$refs.game.height);
+        this.context.fillRect(this.position.x, this.position.y, 20, 20);
+      });
+      
+    },
+    methods: {
+      move (direction) {
+        this.socket.emit("move", direction);
+      }
+    }
+  }
+</script>
+
+
+<style scoped>
+
+</style>
